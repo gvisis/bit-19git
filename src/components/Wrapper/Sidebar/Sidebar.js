1f1029307363fb2928e07b9336f7e9244ade1477
Gvidas <gvisis@gmail.com>
2021-03-10 02:36:58 +0200

    studentai

--- src/components/Wrapper/Sidebar/Sidebar.js
+++ src/components/Wrapper/Sidebar/Sidebar.js
@@ -4,84 +4,22 @@ import './sidebar.css';
 function Sidebar() {
     return (
         <div className='student-list'>
-            <div className='student'>
-                <img src='#' alt='name' />
-                <span>Vardas</span>
-            </div>
-            <div className='student'>
-                <img src='#' alt='name' />
-                <span>Vardas</span>
-            </div>
-            <div className='student'>
-                <img src='#' alt='name' />
-                <span>Vardas</span>
-            </div>
-            <div className='student'>
-                <img src='#' alt='name' />
-                <span>Vardas</span>
-            </div>
-            <div className='student'>
-                <img src='#' alt='name' />
-                <span>Vardas</span>
-            </div>
-            <div className='student'>
-                <img src='#' alt='name' />
-                <span>Vardas</span>
-            </div>
-            <div className='student'>
-                <img src='#' alt='name' />
-                <span>Vardas</span>
-            </div>
-            <div className='student'>
-                <img src='#' alt='name' />
-                <span>Vardas</span>
-            </div>
-            <div className='student'>
-                <img src='#' alt='name' />
-                <span>Vardas</span>
-            </div>
-            <div className='student'>
-                <img src='#' alt='name' />
-                <span>Vardas</span>
-            </div>
-            <div className='student'>
-                <img src='#' alt='name' />
-                <span>Vardas</span>
-            </div>
-            <div className='student'>
-                <img src='#' alt='name' />
-                <span>Vardas</span>
-            </div>
-            <div className='student'>
-                <img src='#' alt='name' />
-                <span>Vardas</span>
-            </div>
-            <div className='student'>
-                <img src='#' alt='name' />
-                <span>Vardas</span>
-            </div>
-            <div className='student'>
-                <img src='#' alt='name' />
-                <span>Vardas</span>
-            </div>
-            <div className='student'>
-                <img src='#' alt='name' />
-                <span>Vardas</span>
-            </div>
-            <div className='student'>
-                <img src='#' alt='name' />
-                <span>Vardas</span>
-            </div>
-            <div className='student'>
-                <img src='#' alt='name' />
-                <span>Vardas</span>
-            </div>
-            <div className='student'>
-                <img src='#' alt='name' />
-                <span>Vardas</span>
-            </div>
+            { studentai}
         </div>
-    )
+    );
+}
+
+function studentai() {
+
+    let allStudents = [];
+    for (let i = 0; i < 5; i++) {
+        allStudents.push(`<a href='#' className='student'>
+        <img src='https://avatars.githubusercontent.com/u/31749032?s=460&u=efe3f0e7d3f9f4be8199f51f1377bc58799b8e31&v=4' alt='name' />
+        <span>Vardas</span>
+        </a>`);
+    }
+    console.log(allStudents);
+    return allStudents;
 }
 
 export default Sidebar;
---
+++ src/components/Wrapper/Sidebar/students.js
@@ -0,0 +1,123 @@
+const students = [
+
+    {
+        name: AndriusK,
+        url: https://github.com/pigma15">Github</a>
+    }
+    {
+        name: Aleksandra,
+        url: https://github.com/alekslov">Github</a>
+    }
+    {
+        name: Andrius,
+        url: https://github.com/Urbbiz">Github</a>
+    }
+    {
+        name: Ana,
+        url: https://github.com/anavisnia">Github</a>
+    }
+    {
+        name: Andrejus,
+        url: https://github.com/andrejusnec">Github</a>
+    }
+    {
+        name: Beata,
+        url: https://github.com/beatarynkevic">Github</a>
+    }
+    {
+        name: Donatas,
+        url: https://github.com/belauzas">Github</a>
+    }
+    {
+        name: Daniel,
+        url: https://github.com/DanielGurevich1">Github</a>
+    }
+    {
+        name: Emilija,
+        url: https://github.com/ESinkev">Github</a>
+    }
+    {
+        name: Eristida,
+        url: https://github.com/Eristid">Github</a>
+    }
+    {
+        name: Gabriele,
+        url: https://github.com/Gabrieleba">Github</a>
+    }
+    {
+        name: Gvidas,
+        url: https://github.com/gvisis">Github</a>
+    }
+    {
+        name: Ježy,
+        url: https://github.com/Gurar">Github</a>
+    }
+    {
+        name: Jokūbas,
+        url: https://github.com/JRybelis"> Github </a>
+    }
+    {
+        name: Laimonas,
+        url: https://github.com/LaimonasMas">Github</a>
+    }
+    {
+        name: Laura,
+        url: https://github.com/laurako14">Github</a>
+    }
+    {
+        name: Mantas,
+        url: https://github.com/bemze">Github</a>
+    }
+    {
+        name: Mindaugas,
+        url: https://github.com/mindep1">Github</a>
+    }
+    {
+        name: Marius,
+        url: https://github.com/2MariusG">Github</a>
+    }
+    {
+        name: Martynas,
+        url: https://github.com/martynas3113">Github</a>
+    }
+    {
+        name: Martyna,
+        url: https://github.com/sezaite">Github</a>
+    }
+    {
+        name: Rimantas,
+        url: https://github.com/front-end-by-rimantas">Github</a>
+    }
+    {
+        name: Rokas J,
+        url: https://github.com/JackeviciusR">Github</a>
+    }
+    {
+        name: Raimundas,
+        url: https://github.com/rokmanas">Github</a>
+    }
+    {
+        name: Rugile,
+        url: https://github.com/rudzyl">Github</a>
+    }
+    {
+        name: Sarunas,
+        url: https://github.com/SarunasCeponis">Github</a>
+    }
+    {
+        name: Tomas,
+        url: https://github.com/Tomas3000">Github</a>
+    }
+    {
+        name: Vaidas,
+        url: https://github.com/vaidas33">Github</a>
+    }
+    {
+        name: Vaida,
+        url: https://github.com/VaidaEG">Github</a>
+    }
+    {
+        name: Zymantas,
+        url: https://github.com/Zymantasdan">Github</a>
+    }
+]
